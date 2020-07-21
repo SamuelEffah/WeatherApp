862d901ec8aa192f8874db1c1cd959b5ff6348f1
SamuelEffah <sameffahjunior@outlook.com>
2020-07-20 21:25:53 -0400

    added backend endpoint

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const OAuth = require("oauth");
+
+dotenv.config({ path: "./.env.local" });
+const port = process.env.PORT || 5000;
+
+const header = {
+  "X-Yahoo-App-Id": process.env.YAHOO_WEATHER_API_APP_ID,
+};
+
+const request = new OAuth.OAuth(
+  null,
+  null,
+  process.env.YAHOO_WEATHER_CLIENT_ID,
+  process.env.YAHOO_WEATHER_CLIENT_SECRET,
+  "1.0",
+  null,
+  "HMAC-SHA1",
+  null,
+  header
+);
+
+const app = express();
+app.get("/:query", (req, res) => {
+  request.get(
+    `https://weather-ydn-yql.media.yahoo.com/forecastrss?location=${req.params.query},ca&format=json`,
+    null,
+    null,
+    function (err, data, result) {
+      if (err) {
+        res.status(400).json({
+            status:"failed"
+        })
+      } else {
+        console.log(data.location)
+        res.status(200).json({
+          status: "success",
+          data
+          
+        });
+      }
+    }
+  );
+});
+
+app.listen(port);
